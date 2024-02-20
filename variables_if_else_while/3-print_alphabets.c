81de8976bedf33661f899405be3e766c2ae64675
samiribrh <samiribrahimov2277@gmail.com>
2024-02-20 11:24:23 +0400

    3rd task

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+*main - Writes alphabet in lowercase then in uppercase.
+*
+*Return: Always 0.
+*/
+int main(void)
+{
+	char n = 'a'
+
+	while (n != '{')
+	{
+		putchar(n);
+		n++;
+	}
+	n = 'A';
+	while ( n != '[')
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
