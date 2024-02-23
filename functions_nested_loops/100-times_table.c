181ebb7e84ad22af263eaa408ccb318dc732b28e
samiribrh <samiribrahimov2277@gmail.com>
2024-02-23 23:18:41 +0400

    12th task sign problem solved 2

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -42,7 +42,7 @@ void print_times_table(int n)
 					_putchar((result / 10) + '0');
 					_putchar((result % 10) + '0');
 				}
-				if (j < n)
+				if (j < n - 1)
 				{
 					_putchar(',');
 					_putchar(' ');
