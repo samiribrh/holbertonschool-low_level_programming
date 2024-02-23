a053c18738d00f819c7cc8bbf53ba89beec99280
samiribrh <samiribrahimov2277@gmail.com>
2024-02-23 22:57:03 +0400

    12th task trial

---
+++ functions_nested_loops/100-times_table.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+*times_table - Writes the n times table.
+*@n: size of the table
+*
+*Return: void.
+*/
+void print_times_table(int n)
+{
+	int i, j, result;
+	
+	if (n > 15 || n < 0)
+		return
+	for (i = 0; i <= n; i++)
+	{
+		for (j = 0; j <= n; j++)
+		{
+			result = i * j;
+
+			if (result < 10)
+			{
+				if (j != 0)
+					_putchar(' ');
+				_putchar(result + '0');
+			}
+			else
+			{
+				_putchar((result / 10) + '0');
+				_putchar((result % 10) + '0');
+			}
+			if (j < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
