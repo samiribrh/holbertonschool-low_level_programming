64bb97666b9b671764157d38c338a611ef674ad2
samiribrh <samiribrahimov2277@gmail.com>
2024-02-24 00:03:40 +0400

    13th task trial 1

---
+++ functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+*main - Writes the sum of multiples of 3 and 5.
+*
+*Return: Always 0.
+*/
+int main(void)
+{
+	int i;
+	int sum = 0;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if ((i % 3 == 0) || (i % 5 == 0))
+		{
+			sum += i;
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
