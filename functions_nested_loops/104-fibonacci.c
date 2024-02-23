c89fedf626b68f52fe4e87f770a5ff754e8342be
samiribrh <samiribrahimov2277@gmail.com>
2024-02-24 00:29:02 +0400

    16th task trial 2

--- functions_nested_loops/104-fibonacci.c
+++ functions_nested_loops/104-fibonacci.c
@@ -7,19 +7,19 @@
 */
 int main(void)
 {
-	long int a1 = 1;
-	long int a2 = 2;
+	unsigned long int a1 = 1;
+	unsigned long int a2 = 2;
 	int i;
-	long int a3;
+	unsigned long int a3;
 
-	printf("%ld, ", a1);
-	printf("%ld", a2);
+	printf("%lu, ", a1);
+	printf("%lu", a2);
 	for (i = 0; i < 96; i++)
 	{
 		a3 = a1 + a2;
 		a1 = a2;
 		a2 = a3;
-		printf(", %ld", a3);
+		printf(", %lu", a3);
 	}
 	printf("\n");
 	return (0);
