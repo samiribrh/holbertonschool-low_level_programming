65fc97b1497a8143f8eb1b8cd2f56e067e5a9696
samiribrh <samiribrahimov2277@gmail.com>
2024-02-23 23:14:22 +0400

    12th task trial

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -16,12 +16,16 @@ void print_times_table(int n)
 
 		for (i = 0; i <= n; i++)
 		{
+			_putchar('#');
 			for (j = 0; j <= n; j++)
 			{
+				_putchar('#');
 				result = i * j;
 
 				if (result < 10)
 				{
+					_putchar('#');
+					
 					if (j != 0)
 					{
 						_putchar(' ');
