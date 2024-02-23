af1bf55d31cfc45996fdcc81c77cf250ba6340a5
samiribrh <samiribrahimov2277@gmail.com>
2024-02-24 00:10:43 +0400

    14th task trial 1

---
+++ functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+*main - Writes the first 50 Fibonacci numbers.
+*
+*Return: Always 0.
+*/
+int main(void)
+{
+	int a1 = 1;
+	int a2 = 2;
+	int i, a3;
+
+	printf("%d, ", a1);
+	printf("%d", a2);
+	for (i = 0; i < 48; i++)
+	{
+		a3 = a1 + a2;
+		a1 = a2;
+		a2 = a3;
+		printf(", ",a3);
+	}
+	return (0);
+}
