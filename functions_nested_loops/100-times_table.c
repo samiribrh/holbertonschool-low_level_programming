3f9a7ae51e68ce2f56ca36891d140deff6598687
samiribrh <samiribrahimov2277@gmail.com>
2024-02-23 23:29:02 +0400

    12th task changed from root

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -9,44 +9,23 @@
 */
 void print_times_table(int n)
 {
-	int i, j, result;
+	int i, j, r, d1, d2, d3;
 
 	if (n <= 15 && n >= 0)
 	{
-
-		for (i = 0; i <= n; i++)
+		for (i = 0; i <= n; ++i)
 		{
-			for (j = 0; j <= n; j++)
+			for (j = 0; j <= n; ++j)
 			{
-				result = i * j;
+				r = i * j;
+				d1 = r % 10 + '0';
+				d2 = (r / 10) % 10 + '0';
+				d3 = (r / 100) + '0';
 
-				if (result < 10)
-				{
-					
-					if (j != 0)
-					{
-						_putchar(' ');
-						_putchar(' ');
-					}
-					_putchar(result + '0');
-				}
-				else if (result < 100)
-				{
-					_putchar(' ');
-					_putchar((result / 10) + '0');
-					_putchar((result % 10) + '0');
-				}
-				else
-				{
-					_putchar((result / 100) + '0');
-					_putchar((result / 10 % 10) + '0');
-					_putchar((result % 10) + '0');
-				}
-				if (j < n)
-				{
-					_putchar(',');
-					_putchar(' ');
-				}
+				if (j != 0) _putchar(','), _putchar(' ');
+				_putchar((r < 10) ? ' ' : (r < 100) ? ' ' : d3);
+				_putchar((r < 10) ? ' ' : (r < 100) ? d2 : d2);
+				_putchar(d1);
 			}
 			_putchar('\n');
 		}
