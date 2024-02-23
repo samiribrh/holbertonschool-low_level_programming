ec099f21b97da7eecffba6a8bd7ef2822eb9e4fa
samiribrh <samiribrahimov2277@gmail.com>
2024-02-24 01:08:23 +0400

    16th task trial 3

--- functions_nested_loops/104-fibonacci.c
+++ functions_nested_loops/104-fibonacci.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
-*main - Writes the first 50 Fibonacci numbers.
+*main - Writes the first 98 Fibonacci numbers.
 *
 *Return: Always 0.
 */
@@ -10,16 +10,29 @@ int main(void)
 	unsigned long int a1 = 1;
 	unsigned long int a2 = 2;
 	int i;
-	unsigned long int a3;
+	int lnum = 1000000000;
+	unsigned long int a3, part1, part2;
 
 	printf("%lu, ", a1);
 	printf("%lu", a2);
 	for (i = 0; i < 96; i++)
 	{
-		a3 = a1 + a2;
-		a1 = a2;
-		a2 = a3;
-		printf(", %lu", a3);
+		if (a1 + a2 > lnum)
+		{
+			part1 = (a1 + a2) / lnum;
+			part2 = (a1 + a2) % lnum;
+			a3 = a1 + a2;
+			a1 = a2;
+			a2 = a3;
+			printf("%lu%010lu", part1, part2);
+		}
+		else
+		{
+			a3 = a1 + a2;
+			a1 = a2;
+			a2 = a3;
+			printf(", %lu", a3);
+		}
 	}
 	printf("\n");
 	return (0);
