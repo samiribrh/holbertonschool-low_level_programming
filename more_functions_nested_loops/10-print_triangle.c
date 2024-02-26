f3a1354e3892001988d78cb0f47292df4a61174e
samiribrh <samiribrahimov2277@gmail.com>
2024-02-26 11:33:35 +0400

    10th task wrong semicolon removed

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -15,7 +15,7 @@ void print_triangle(int size)
 		{
 			for (s = 0; s < size - h; s++)
 				_putchar(' ');
-			for (n = 0; n < h; n++);
+			for (n = 0; n < h; n++)
 				_putchar('#');
 			_putchar('\n');
 			h++;
