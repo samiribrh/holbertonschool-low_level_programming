09bdd524c1c2fac3102be109c4b22bf95f86b37b
samiribrh <samiribrahimov2277@gmail.com>
2024-02-26 10:08:21 +0400

    directory name corrected

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+*_isupper - Checks the characther for uppercase.
+*@c: Characther to be checked
+*
+*Return: 1 if c is uppercase
+*	0 otherwise
+*/
+int _isupper(int c);
+{
+	if (c > 64 && c < 91)
+		return (1);
+	else
+		return (0);
+}
---
+++ more_functions_nested_loops/README.md
@@ -0,0 +1,3 @@
+**Overview:**
+
+This directory consists of files that includes script writeen in C language and uses nested loops and functions.
