a55afc85b068f9239322aa165e0fcf26c04f2597
samiribrh <samiribrahimov2277@gmail.com>
2024-02-26 11:21:34 +0400

    7th task minor bugs solved

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -11,10 +11,12 @@ void print_diagonal(int n)
 	int i, s;
 	if (n > 0)
 		for (i = 0; i < n; i++)
+		{
 			for (s = 0; s < i; s++)
 				_putchar(' ');
-			_putchar('\');
+			_putchar('\\');
 			_putchar('\n');
+		}
 	else
 		_putchar('\n');
 }
