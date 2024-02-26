f05412a25b1b3bf4d0587a982c97f2de233b6025
samiribrh <samiribrahimov2277@gmail.com>
2024-02-26 12:04:57 +0400

    101th task trial 1

---
+++ more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+*print_number - Writes the given integer.
+*@n: the integer that should be printed.
+*
+*Return: void.
+*/
+void main(int n)
+{
+	void print_number(int n)
+	{
+		int i = 1;
+
+		if (n < 0)
+		{
+			n = -n;
+			_putchar('-');
+		}
+		while ((n / i) / 10 != 0)
+		{
+			i *= 10;
+		}
+		for (; i != 1; i /= 10)
+		{
+			_putchar((n / i) + '0');
+			n = n % i;
+		}
+		_putchar((n % 10) + '0');
+	}
+}
