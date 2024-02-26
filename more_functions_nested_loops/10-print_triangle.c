3eea9935a968de88d2af2425005dcf17b853bbc3
samiribrh <samiribrahimov2277@gmail.com>
2024-02-26 11:09:15 +0400

    9-10 tasks trial 1

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+*print_triangle - Draws triangle.
+*@size: size of the triangle
+*
+*Return: void.
+*/
+void print_triangle(int size)
+{
+	int s, n, h = 0;
+	if (size > 0)
+	{
+		while (h < size)
+		{
+			for (s = 0; s < size - h; s++)
+				_putchar(' ');
+			for (n = 0; n < h; n++);
+				_putchar('#');
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+*print_numbers - Writes numbers from 1 to 100,
+*	but Fizz instead of multiples of 3,
+*	and Buzz instead of multiples of 5.
+*
+*Return: Always 0;
+*/
+int main(void)
+{
+	int n = 1;
+
+	while (i < 101)
+	{
+		if (n % 3 == 0)
+			printf("Fizz");
+		else if (n % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%d", n);
+		if(n != 100)
+			printf(" ");
+
+	}
+	printf("\n");
+	return (0);
+}
