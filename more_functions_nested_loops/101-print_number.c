53c8795a62b07e4e674a9a2e37164a353897b062
samiribrh <samiribrahimov2277@gmail.com>
2024-02-26 12:06:31 +0400

    101th task trial 2

--- more_functions_nested_loops/101-print_number.c
+++ more_functions_nested_loops/101-print_number.c
@@ -6,26 +6,24 @@
 *
 *Return: void.
 */
-void main(int n)
+void print_number(int n)
 {
-	void print_number(int n)
-	{
-		int i = 1;
+	int i = 1;
 
-		if (n < 0)
-		{
-			n = -n;
-			_putchar('-');
-		}
-		while ((n / i) / 10 != 0)
-		{
-			i *= 10;
-		}
-		for (; i != 1; i /= 10)
-		{
-			_putchar((n / i) + '0');
-			n = n % i;
-		}
-		_putchar((n % 10) + '0');
+	if (n < 0)
+	{
+		n = -n;
+		_putchar('-');
 	}
+	while ((n / i) / 10 != 0)
+	{
+		i *= 10;
+	}
+	for (; i != 1; i /= 10)
+	{
+		_putchar((n / i) + '0');
+		n = n % i;
+	}
+	_putchar((n % 10) + '0');
 }
+
