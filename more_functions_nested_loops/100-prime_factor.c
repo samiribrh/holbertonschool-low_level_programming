ee5dadd3459eac317b01702f7ae6a6c8cabf1529
samiribrh <samiribrahimov2277@gmail.com>
2024-02-26 11:46:44 +0400

    100th task trial 1

---
+++ more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+*main - Writes the biggest prime factor of the number 612852475143.
+*
+*Return: Always 0.
+*/
+int main(void)
+{
+	long int i = 0, n = 612852475143;
+
+	while (i != n)
+	{
+		if (n % i == 0)
+		{
+			n /= i;
+			continue;
+		}
+		i++;
+	}
+	printf("%ld", n);
+	return (0);
+}
