4a202499ba404296f5d1c23e29e061faf3b7475f
samiribrh <samiribrahimov2277@gmail.com>
2024-02-27 18:37:50 +0400

    9th task missing i++ added

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -11,10 +11,10 @@ char *_strcpy(char *dest, char *src)
 {
 	int i = 0;
 
-	while (src[i - 1] != '\0')
+	while (src[i] != '\0')
 	{
-		dest[i] = src[i];
 		i++;
+		dest[i] = src[i];
 	}
 	return (dest);
 }
