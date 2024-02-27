1d5b723ad585fda5919cda524fcaee983d6a2bbb
samiribrh <samiribrahimov2277@gmail.com>
2024-02-27 20:54:04 +0400

    101th task finally

--- pointers_arrays_strings/101-keygen.c
+++ pointers_arrays_strings/101-keygen.c
@@ -3,23 +3,30 @@
 #include <stdlib.h>
 
 /**
-*main - Generates 50 digit random password.
+*main - Generates  random password for 101-creckme program.
 *
 *Return: void.
 */
 int main(void)
 {
-	int pass[50];
-	int digit;
+	int pass[100];
+	int digit, sum = 0;
 
 	srand(time(NULL));
 
-	for (digit = 0; digit < 50; digit++)
+	for (digit = 0; digit < 100; digit++)
 	{
 		pass[digit] = rand() % 78;
 		putchar(pass[digit] + '0');
+		sum += pass[digit];
+		if (2772 - sum - '0' < 78 )
+		{
+			n = 2772 - sum - '0';
+			sum += n;
+			putchar(n + '0');
+			break;
+		}
 	}
-	putchar('\n');
 	return (0);
 }
 
