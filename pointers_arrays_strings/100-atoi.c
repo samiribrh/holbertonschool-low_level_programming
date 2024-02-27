e13a33a7dee7a57f3e7ac40ca6d794b64b748a33
samiribrh <samiribrahimov2277@gmail.com>
2024-02-27 19:04:21 +0400

    10th task changed

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -11,12 +11,16 @@ int _atoi(char *s)
 	int sign = 1;
 	int result = 0;
 
-	while(*s && ((*s >= '0' && *s <= '9') || (*s == '-') || (*s == '+')))
+	while(*s)
 	{
 		if (*s == '-')
 			sign *= -1;
+		else if (*s == '+')
+			continue;
 		else if (*s >= '0' && *s <= '9')
-			result = result * 10 + (*s - '0');
+			result = result * 10 + *s - '0';
+		else if (result != 0)
+			break;
 		s++;
 	}
 	return (result * sign);
