ffc9dcf5f6dbfdf13adc44431f3c550e555949eb
samiribrh <samiribrahimov2277@gmail.com>
2024-02-27 19:42:20 +0400

    101st task trial

---
+++ pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <time.h>
+
+/**
+*main - Generates 25 digit random password.
+*
+*Return: void.
+*/
+int main(void)
+{
+	int pass[25];
+	int digit;
+
+	srand(time(NULL));
+
+	for (digit = 0; digit < 25; digit++)
+	{
+		pass[digit] = rand() % 78;
+		putchar(pass[digit] + '0');
+	}
+	putchar('\n');
+	return (0);
+}
+
