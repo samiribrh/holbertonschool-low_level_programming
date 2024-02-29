0a230db438d04f6175c658a6acb402658ca63f4d
samiribrh <samiribrahimov2277@gmail.com>
2024-02-29 10:22:52 +0400

    1st task trial 1

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+**_strcat -  Appends two strings.
+*@dest: Destination string
+*@src: String to be appended
+*@n: Size of the part of src which should be appended
+*
+*Return: Appended string.
+*/
+char *_strncat(char *dest, char *src, int n)
+{
+	int d = 0, i = 0;
+
+	while (dest[d] != 0)
+		d++;
+	while ((src[i] != 0) && (i < n))
+	{
+		dest[d] = src[i];
+		d++;
+		i++;
+	}
+	dest[d] = 0;
+	return (dest);
+}
