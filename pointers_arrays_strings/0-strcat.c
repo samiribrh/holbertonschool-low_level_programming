b092a821ea6b9affdab83b3143984c5b775b2085
samiribrh <samiribrahimov2277@gmail.com>
2024-02-29 10:16:02 +0400

    0th task trial 10

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -11,11 +11,11 @@ char *_strcat(char *dest, char *src)
 {
 	int d = 0, i = 0;
 
-	while (dest[d] != 0)
+	while (*(dest + d) != 0)
 		d++;
 	while (src[i] != 0)
 	{
-		dest[d] = src[i];
+		*(dest + d) = *(src + i);
 		d++;
 		i++;
 	}
