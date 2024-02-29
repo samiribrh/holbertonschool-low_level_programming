29e4c5bdc88f3361f583a7451ad0df9588f5e3a8
samiribrh <samiribrahimov2277@gmail.com>
2024-02-29 09:46:22 +0400

    0th task trial

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+**_strcat -  Appends two strings.
+*@dest: Destination string
+*@src: String to be appended
+*
+*Return: Appended string.
+*/
+char *_strcat(char *dest, char *src)
+{
+	int i = 0;
+
+	while (*dest)
+		i++;
+		dest++;
+	while (*src)
+		dest[i] = *src;
+		src++;
+	return (dest);
+}
