4da1bd1cfc59c476647118cd8a2eb465e6879d93
samiribrh <samiribrahimov2277@gmail.com>
2024-03-01 20:10:19 +0400

    cap_string trial

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+**cap_string - Capitalizes the words in string.
+*@s: String to be changed
+*
+*Return: Capitalized string.
+*/
+char *cap_string(char *s)
+{
+	int i = 0;
+	char t;
+
+	while (s[i])
+	{
+		t = s[i - 1];
+		if (s[i] >= 97 && s[i] <= 122)
+		{
+			if (t == ' ' || t == '\t' || t == '\n' || t == ',' || t == ';' || t == '.')
+				s[i] -= 32;
+
+			if (t == '!' || t == '?' || t == '(' || t == ')' || t == '{' || t == '}')
+				s[i] -= 32;
+
+			if (t == '"' || i == 0)
+				s[i] -= 32;
+		}
+		i++;
+	}
+	return (s);
+}
