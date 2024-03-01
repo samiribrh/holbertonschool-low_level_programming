8516491c8cf8a2c4dc30f076742224c7a44217aa
samiribrh <samiribrahimov2277@gmail.com>
2024-03-01 19:37:37 +0400

    strcmp trial 2

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -12,13 +12,10 @@ int _strcmp(char *s1, char *s2)
 {
 	int i = 0;
 
-	while ((s1[i] != 0) && (s2[i] != 0))
+	while (*s1 && (*s1 == *s2))
 	{
-		if (s1[i] != s2[i])
-			return 0;
-		i++;
+		s1++;
+		s2++;
 	}
-	if ((s1[i] == 0) ^ (s2[i] == 0))
-		return 0;
-	return 1;
+	return *s1 - *s2;
 }
