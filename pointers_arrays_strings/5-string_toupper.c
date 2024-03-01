4a427b4e24f3bfd846d8f92084cee78107d7d02c
samiribrh <samiribrahimov2277@gmail.com>
2024-03-01 19:54:53 +0400

    strin_toupper trial

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+**string_toupper - Changes the lowercase letters of a string to upper.
+*@s: String to be changed
+*
+*Return: Uppercased string.
+*/
+char *string_toupper(char *s)
+{
+	while (*s)
+	{
+		if (*s >= 'a' && *s <= 'z')
+			*s -= 32;
+		s++;
+	}
+	return (s);
+}
