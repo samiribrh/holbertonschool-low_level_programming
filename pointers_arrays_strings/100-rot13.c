603fbe755b4a95b6a2110dc5a5d8a6ba900143d0
samiribrh <samiribrahimov2277@gmail.com>
2024-03-02 00:05:41 +0400

    Rot13 encyrption trial 4

--- pointers_arrays_strings/100-rot13.c
+++ pointers_arrays_strings/100-rot13.c
@@ -8,16 +8,20 @@
 */
 char *rot13(char *s)
 {
-	int i, key = 13;
+	int i;
 
 	for (i = 0; s[i] != 0; i++)
 	{
-		if (s[i] + key > 122)
+		while ((s[i] >= 65 && s[i] <= 90) || (s[i] >= 97 && s[i] <= 122))
 		{
-			s[i] -= 13;
-			continue;
+			if ((s[i] >= 65 && s[i] <= 77) || (s[i] >= 97 && s[i] <= 109))
+			{
+				s[i] = s[i] + 13;
+				break;
+			}
+			s[i] = s[i] - 13;
+			break;
 		}
-		s[i] += key;
 	}
 	return (s);
 }
