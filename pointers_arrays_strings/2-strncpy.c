0b1eca942d36dc8152d0b4b63fb5e6cda0848ddb
samiribrh <samiribrahimov2277@gmail.com>
2024-03-01 19:23:08 +0400

    improved strncpy

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -10,16 +10,14 @@
 */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i = 0, d = 0;
+	int i = 0;
 
-	while (dest[d] != '\0')
-		d++;
 	while ((src[i] != '\0') && (i < n))
 	{
 		dest[i] = src[i];
 		i++;
 	}
-	while (i < d)
+	while (i < n)
 	{
 		dest[i] = '\0';
 		i++;
