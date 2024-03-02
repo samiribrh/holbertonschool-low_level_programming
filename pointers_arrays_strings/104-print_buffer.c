b71b26449560b70584b976bfd237b82036881fe4
samiribrh <samiribrahimov2277@gmail.com>
2024-03-02 20:37:39 +0400

    Print buffer from zero

--- pointers_arrays_strings/104-print_buffer.c
+++ pointers_arrays_strings/104-print_buffer.c
@@ -3,44 +3,33 @@
 void print_buffer(char *b, int size)
 {
 	int i, j;
-	char current_char;
 
 	if (size <= 0)
 	{
 		printf("\n");
 		return;
 	}
-
 	for (i = 0; i < size; i += 10)
 	{
-		printf("%08x: ", i);
+		printf("%08x: ",i);
 		for (j = 0; j < 10; j++)
 		{
 			if (i + j < size)
 			{
-				printf("%02x", (unsigned char)b[i + j]);
+				printf("%02x", b[i+j]);
 			}
 			else
 			{
-				printf("  ");
-			}
-
-			if (j % 2 == 1)
-			{
-				printf(" ");
+				printf("    ");
 			}
 		}
-		for (j = 0; j < 10 && i + j < size; j++) 
+		printf(" ");
+		for (j = 0; j < 10 && i + j < size; j++)
 		{
-			current_char = b[i + j];
-			if (current_char >= 32 && current_char <= 126)
-			{
-				printf("%c", current_char);
-			}
+			if (b[i+j] >= 32 && b[i+j] <= 126)
+				printf("%c", b[i+j]);
 			else
-			{
-				printf(".");
-			}
+				printf(".")
 		}
 		printf("\n");
 	}
