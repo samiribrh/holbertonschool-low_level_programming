34948d0442760281e146ea4f452b122a9b7c4a7d
samiribrh <samiribrahimov2277@gmail.com>
2024-03-04 11:24:03 +0400

    strstr trial

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+/**
+**_strstr - Searches the string for provided string.
+*@haystack: The string to be searched
+*@needle: Accepted string
+*
+*Return: Pointer to the beginning location of the first occurence of the string.
+*/
+char *_strstr(char *haystack, char *needle)
+{
+	int i, found;
+
+	while (*haystack != '\0')
+	{
+		for (i = 0; needle[i] != '\0'; i++)
+		{
+			if (*(haystack + i) == needle[i])
+			{
+				if (accept[i + 1] != '\0')
+					continue;
+				else
+					return (haystack);
+			}
+			else
+			{
+				break;
+			}
+		}
+		haystack++;
+	}
+	return (NULL);
+}
