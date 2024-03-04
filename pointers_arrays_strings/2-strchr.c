e8d332440fec90225c9b7f75cb4bf157b46e4d78
samiribrh <samiribrahimov2277@gmail.com>
2024-03-04 10:16:33 +0400

    strchr terminator added

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -10,7 +10,7 @@
 */
 char *_strchr(char *s, char c)
 {
-	while (*(s - 1) != '\0')
+	while (*s != '\0')
 	{
 		if (*s == c)
 			return (s);
@@ -19,5 +19,7 @@ char *_strchr(char *s, char c)
 			s++;
 		}
 	}
+	if (c == '\0')
+		return (s);
 	return (NULL);
 }
