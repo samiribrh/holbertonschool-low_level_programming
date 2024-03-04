0cb5539b3e1feca41d95f3d47747834965d45d1a
samiribrh <samiribrahimov2277@gmail.com>
2024-03-04 10:11:38 +0400

    strchr trial

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+**_strchr - Locates first occurence of a characther in a string.
+*@s: The string to be searched 
+*@c: The character to be searched for
+*
+*Return: A pointer to the first occurence of character.
+*/
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+			return (s);
+		else
+		{
+			s++;
+		}
+	}
+	return (NULL);
+}
