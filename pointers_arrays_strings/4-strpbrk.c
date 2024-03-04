faf0abe0601b6c86bf1055de5c62dc077a7bf77f
samiribrh <samiribrahimov2277@gmail.com>
2024-03-04 11:10:57 +0400

    brackets added

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 /**
-*_strspn - Searches the string for any character from provided string.
+**_strpbrk - Searches the string for any character from provided string.
 *@s: The string to be searched
 *@accept: Accepted characters
 *
@@ -14,8 +14,12 @@ char *_strpbrk(char *s, char *accept)
 	while (*s != '\n')
 	{
 		for (i = 0; accept[i] != '\0'; i++)
+		{
 			if (*s == accept[i])
+			{
 				return (s);
+			}
+		}
 		s++;
 	}
 	return (NULL);
