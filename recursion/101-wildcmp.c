704af29b84dd3698416658a62a22e7cc581939ab
samiribrh <samiribrahimov2277@gmail.com>
2024-03-05 22:25:34 +0400

    wildcard option added

--- recursion/101-wildcmp.c
+++ recursion/101-wildcmp.c
@@ -13,7 +13,7 @@ int wildcmp(char *s1, char *s2)
 	if (!(*s1) && !(*s2))
 		return (1);
 	else if (*s1 == *s2)
-		return (wildcmp(s1 + 1, s2 + 1)) 
+		return (wildcmp(s1 + 1, s2 + 1));
 	else if (*s2 == '*')
 		return (wildcmp(s1 + 1, s2));
 	return 0;
