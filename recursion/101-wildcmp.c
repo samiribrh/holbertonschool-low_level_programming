88beab73b3c463d15ae9c0a9ccd454e721c10da6
samiribrh <samiribrahimov2277@gmail.com>
2024-03-05 22:09:45 +0400

    wildcmp trial

---
+++ recursion/101-wildcmp.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+*wildcmp - Compares two strings.
+*@s1: The first string
+*@s2: The second string
+*
+*Return: 1 if strings are identical
+*	0 if strings are not identical.
+*/
+int wildcmp(char *s1, char *s2)
+{
+	if (!(*s1) && !(*s2))
+		return (1);
+	else if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	return 0;
+}
