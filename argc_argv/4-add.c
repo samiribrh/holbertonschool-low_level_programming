093a9a7c50ac4c9cd78b354448a9fefbde1ca597
samiribrh <samiribrahimov2277@gmail.com>
2024-03-07 20:03:05 +0400

    add trial

---
+++ argc_argv/4-add.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+*main - Adds positive numbers.
+*@argc: Number of arguments
+*@argv: The array of pointers to the arguments
+*
+*Return: 0 if success
+*	1 if Error.
+*/
+int main(int argc, int **argv)
+{
+	int j, i = 1, sum = 0;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+	else
+	{
+		while (i < argc)
+		{
+			j = 0;
+			while (j < strlen(argv[i]))
+			{
+				if (argv[i][j] < 48 || argv[i][j] > 57)
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
+			sum = sum + atoi(argv[i]);
+			i++;
+		}
+		printf("%d\n", sum);
+	}
+	return (0);
+}
