3daa9453e3b04b235e7bf4d5b987d7651d6518d9
samiribrh <samiribrahimov2277@gmail.com>
2024-03-07 10:28:32 +0400

    betty and betty documentation style applied

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 
+/**
+*main - Prints the program's name.
+*@argc: Count of the arguments
+*@argv: Array of the pointers to the arguments.
+*
+*Return: Always 0.
+*/
 int main(__attribute((unused)) int argc, char **argv)
 {
 	printf("%s\n", argv[0]);
-	return 0;
+	return (0);
 }
