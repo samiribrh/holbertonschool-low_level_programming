76106d0b417872ea8a75c4016e3a9d383d913534
samiribrh <samiribrahimov2277@gmail.com>
2024-03-16 11:38:37 +0400

    Extra memory space removed

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -27,7 +27,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (n >= l2)
 		n = l2;
 
-	str = malloc(sizeof(char) * (l1 + l2 + 1));
+	str = malloc(sizeof(char) * (l1 + n + 1));
 	if (str == NULL)
 		return NULL;
 
