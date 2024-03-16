7b12f17ebe8c7407e03721262ac2f787e46f3414
samiribrh <samiribrahimov2277@gmail.com>
2024-03-16 11:59:07 +0400

    Betty style applied

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -14,10 +14,10 @@ int *array_range(int min, int max)
 {
 	int *arr;
 	int i;
-	
+
 	if (min > max)
 		return (NULL);
-	
+
 	arr = malloc(sizeof(int) * (max - min + 1));
 	if (arr == NULL)
 		return (NULL);
