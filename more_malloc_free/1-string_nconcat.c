51c8b50ea74a7b230445336e22b1bffc909f314f
samiribrh <samiribrahimov2277@gmail.com>
2024-03-16 11:36:31 +0400

    missing conditions added

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -16,6 +16,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int l1 = 0, l2 = 0, i = 0;
 	char *str;
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	for (l1 = 0; *(s1 + l1) != 0; l1++)
 		;
 	for (l2 = 0; *(s2 + l2) != 0; l2++)
