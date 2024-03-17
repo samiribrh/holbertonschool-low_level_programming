7a77ee2bba7fb39849fb4bc40947de1aa0dce1a4
samiribrh <samiribrahimov2277@gmail.com>
2024-03-17 22:27:42 +0400

    realloc trial

---
+++ more_malloc_free/100-realloc.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+*_realloc - Reallocates memory.
+*@ptr: Previous memory
+*@old_size: Size of the previous memory
+*@new_size: Desired new size
+*
+*Return: Pointer to the new memory,
+*	NULL if fails.
+*/
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *newloc;
+	int i = 0, our_size;
+
+	if (new_size == old_size)
+		return (ptr);
+	else if (!new_size && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	newloc = malloc(new_size);
+	if (newloc == NULL)
+		return (NULL);
+
+	if (new_size >= old_size)
+		our_size = old_size;
+	else
+		our_size = new_size;
+	while (ptr != NULL && i < our_size)
+	{
+		*(newloc + i) = *(ptr + i)
+		i++;
+	}
+	free(ptr);
+
+	return (newloc);
+}
