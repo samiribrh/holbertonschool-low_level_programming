395a172b78dab8a4c6a392a4d63cc92cc0f4e165
samiribrh <samiribrahimov2277@gmail.com>
2024-03-19 22:36:33 +0400

    5-6th tasks

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+*new_dog - Creates a struct dog.
+*@name: Name of the dog
+*@age: Age of the dog
+*@owner: Owner of the dog
+*
+*Return: The structed variable,
+*	NULL if fails.
+*/
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *dog;
+	int lenName, lenOwner;
+
+	lenName = strlen(name) + 1;
+	lenOwner = strlen(owner) + 1;
+
+	dog = malloc(sizeof(dog_t));
+	if (dog == NULL)
+		return (NULL);
+
+	dog->name = malloc(lenName);
+	if (dog->name == NULL)
+	{
+		free(dog->name);
+		free(dog);
+		return (NULL);
+	}
+
+	dog->owner = malloc(lenOwner);
+	if (dog->owner == NULL)
+	{
+		free(dog->name);
+		free(dog->owner);
+		free(dog);
+		return (NULL);
+	}
+
+	strcpy(dog->name, name);
+	dog->age = age;
+	strcpy(dog->owner, owner);
+	return (dog);
+}
---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+*init_dog - Frees struct dog.
+*@d: Structure
+*
+*Return: void.
+*/
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+	else
+		return (NULL);
+}
