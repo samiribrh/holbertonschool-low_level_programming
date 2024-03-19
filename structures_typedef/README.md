174e54a3864c3a28d8af0252b19fa3c9b03b2c77
samiribrh <samiribrahimov2277@gmail.com>
2024-03-19 21:35:39 +0400

    dog structure

---
+++ structures_typedef/README.md
@@ -0,0 +1,3 @@
+**Overview:**
+
+This directory consists of files that contains scripts written in C which includes structures and typedef.
---
+++ structures_typedef/dog.h
@@ -0,0 +1,14 @@
+/**
+*dog - Defines a dog.
+*@name: Name of the dog
+*@age: Age of the dog
+*@owner: Owner of the dog
+*
+*Description: Defines a dog with its name, age and owner.
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
