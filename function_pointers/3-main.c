631f18129b2d77c71260d7256f6c9c2d7c24ecca
samiribrh <samiribrahimov2277@gmail.com>
2024-03-22 17:19:40 +0400

    Layout imrpoved

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -21,22 +21,25 @@ int main(int argc, char **argv)
 		printf("Error\n");
 		exit(98);
 	}
-	
+
 	num1 = atoi(argv[1]);
-	num2 = atoi(argv[3]);
 	op = argv[2];
-	
+	num2 = atoi(argv[3]);
+
 	if (get_op_func(op) == NULL || op[1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
 	}
 
-	if ((*op == '/' || *op == '%') && num2 == 0)
+	if ((*op == '/' && num2 == 0) ||
+	    (*op == '%' && num2 == 0))
 	{
 		printf("Error\n");
 		exit(100);
 	}
+
 	printf("%d\n", get_op_func(op)(num1, num2));
+
 	return (0);
 }
