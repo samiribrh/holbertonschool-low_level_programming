369bd69f6a6bd5c940e4f08d2057f9450f1499ae
samiribrh <samiribrahimov2277@gmail.com>
2024-03-22 18:29:24 +0400

    Change in way

--- variadic_functions/100-hello_world.asm
+++ variadic_functions/100-hello_world.asm
@@ -2,10 +2,11 @@ section .data
 	msg db "Hello, World!", 0xA
 	msg_len equ $ - msg
 
-section .text
-	global _start
+global main
 
-_start:
+section .text
+
+main:
 	mov rax, 1
 	mov rdi, 1
 	mov rsi, msg
