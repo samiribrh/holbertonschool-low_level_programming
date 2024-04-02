457159a5761dce995a6826b8b464eb00302c3fc9
samiribrh <samiribrahimov2277@gmail.com>
2024-04-02 11:41:14 +0400

    Initital list_len func

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+*list_len - Calculates number of elements in the given list.
+*@h: Given list
+*
+*Return: The number of elements.
+*/
+size_t list_len(const list_t *h);
+{
+	unsigned int count = 0;
+
+	while (h != NULL)
+	{
+		count++;
+		if (h->next == NULL)
+			break;
+		h = h->next;
+	}
+	return (count);
+}
