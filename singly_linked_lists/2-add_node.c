e8c4cdbb130c54c40e5c0d5dabe0a47618e56d57
samiribrh <samiribrahimov2277@gmail.com>
2024-04-02 15:47:17 +0400

    Forbidden function substituted

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -13,13 +13,17 @@
 */
 list_t *add_node(list_t **head, const char *str)
 {
+	int len = 0;
 	list_t *new = malloc(sizeof(list_t));
 
 	if (new == NULL)
 		return (NULL);
 
+	while (*(str + len))
+		len++;
+
 	new->str = strdup(str);
-	new->len = strlen(str);
+	new->len = len;
 	new->next = *head;
 	*head = new;
 	return (new);
