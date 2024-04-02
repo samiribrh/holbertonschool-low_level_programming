d4e5d752b69cfd93bd45123ac05e2c860ff6b7f7
samiribrh <samiribrahimov2277@gmail.com>
2024-04-02 11:17:21 +0400

    print_list initial trial

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+*print_list - Writes the given list.
+*@h: Given list
+*
+*Return: The number of nodes.
+*/
+size_t print_list(const list_t *h)
+{
+	unsigned int count = 0;
+	
+	while (h->next != NULL)
+	{
+		if (*(h->str) != NULL)
+			printf("[%u] %s", h->len, h->str);
+		else
+			printf("[0] (nil)");
+		count++;
+		h = *(h->next);
+	}
+	return (count);
+}
