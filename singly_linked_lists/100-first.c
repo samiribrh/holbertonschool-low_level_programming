36e63709fe6bea117dccee2a93871bbc99f7a38a
samiribrh <samiribrahimov2277@gmail.com>
2024-04-02 19:42:39 +0400

    100-first trial

---
+++ singly_linked_lists/100-first.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+void __attribute__ ((constructor)) premain()
+{
+    printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
