f0acb91e2c6325e0befa014f4fad242cac84c3bf
samiribrh <samiribrahimov2277@gmail.com>
2024-04-05 00:50:28 +0400

    Hotfix

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -19,7 +19,7 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 		head = head->next;
 		i++;
 	}
-	if (i == index)
+	if (i == index + 1)
 		return (head);
 	return (NULL);
 }
