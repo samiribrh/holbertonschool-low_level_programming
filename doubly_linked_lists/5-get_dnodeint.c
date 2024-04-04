7151e21752764031806d8cc57318d8d1e519974d
samiribrh <samiribrahimov2277@gmail.com>
2024-04-05 00:51:18 +0400

    Condition changed for correct handling

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -14,7 +14,7 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	unsigned int i = 0;
 
-	while (i < index + 1 && head)
+	while (i < index && head)
 	{
 		head = head->next;
 		i++;
