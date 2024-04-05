6c2ee07a1839b2cdb5ea603039ed6d7fddc55eb2
samiribrh <samiribrahimov2277@gmail.com>
2024-04-05 22:37:41 +0400

    binary to uint trial

---
+++ bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - Converts binary to unsigned int.
+ * @b: Binary number
+ *
+ * Return: The converted number,
+ *	0 if fails.
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int number = 1;
+	int len = 0, started = 0, start;
+
+	if (b == NULL)
+		return (NULL);
+	while (*(b + len))
+	{
+		if (*(b + len) != 1 || *(b + len) != 0)
+			return (0);
+		if (*(b + len) == 1 && !started)
+		{
+			started = 1;
+			start = len + 1;
+		}
+		len++;
+	}
+	while (start <= len)
+	{
+		if (*(b + start) == 0)
+			number *= 2;
+		else
+			number = number * 2 + 1;
+		start++;
+	}
+	return (number);
+}
---
+++ bit_manipulation/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdarg.h>
+
+int _putchar(char c);
+
+#endif
