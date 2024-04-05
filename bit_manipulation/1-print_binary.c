3e4c28c98b7f3c624809f7663cb6f5943cf72969
samiribrh <samiribrahimov2277@gmail.com>
2024-04-05 23:32:11 +0400

    binary printing trial

---
+++ bit_manipulation/1-print_binary.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_binary - Prints the binary representation of a number.
+ * @n: Number that should be converted
+ *
+ * Return: void.
+ */
+void print_binary(unsigned long int n)
+{
+	int buffer[32];
+	int i = 0, j;
+	if (n == 0)
+	{
+		printf("0");
+		return:
+	}
+	while (n > 0)
+	{
+		binary[i] = n % 2;
+		n = n / 2;
+		i++;
+	}
+	for (j = i - 1; j >= 0; j--)
+		printf("%d", buffer[j]);
+}
