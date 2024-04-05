e49b366a909577ae7b1d3b13840df6559ae1c36b
samiribrh <samiribrahimov2277@gmail.com>
2024-04-05 22:42:00 +0400

    missing asteriks added

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -12,7 +12,7 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int number = 1;
 	int len = 0, started = 0, start;
 
-	if (b == NULL)
+	if (*b == NULL)
 		return (0);
 	while (*(b + len))
 	{
