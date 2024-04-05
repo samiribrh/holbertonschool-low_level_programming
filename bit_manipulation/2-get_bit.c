4220431178063d8e18c7a8b8eb413af1876dcdbf
samiribrh <samiribrahimov2277@gmail.com>
2024-04-05 23:51:55 +0400

    get_bit trial

---
+++ bit_manipulation/2-get_bit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * get_bit - Gets the bit at index.
+ * @n: The number
+ * @index: The index
+ *
+ * Return: Number at index,
+ *	-1 if fails.
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index >= sizeof(n) * 8)
+		return (-1);
+	return (n >> index) & 1;
+}
