288ac2ed053a6a5ef533fb5fcb80262fa61529f8
samiribrh <samiribrahimov2277@gmail.com>
2024-04-05 23:38:42 +0400

    Changed to recursion

--- bit_manipulation/1-print_binary.c
+++ bit_manipulation/1-print_binary.c
@@ -1,6 +1,17 @@
 #include "main.h"
 
 /**
+ *
+ */
+void print_num(int n)
+{
+	if (n == 0)
+		return;
+	print_num(n / 2)
+	printf("%d", n % 2);
+}
+
+/**
  * print_binary - Prints the binary representation of a number.
  * @n: Number that should be converted
  *
@@ -8,19 +19,8 @@
  */
 void print_binary(unsigned long int n)
 {
-	int buffer[32];
-	int i = 0, j;
 	if (n == 0)
-	{
 		printf("0");
-		return;
-	}
-	while (n > 0)
-	{
-		buffer[i] = n % 2;
-		n = n / 2;
-		i++;
-	}
-	for (j = i - 1; j >= 0; j--)
-		printf("%d", buffer[j]);
+	else
+		print_num(n);
 }
