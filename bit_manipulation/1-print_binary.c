913dceac57a388b5caaf6f37f5e946e3e7196424
samiribrh <samiribrahimov2277@gmail.com>
2024-04-05 23:42:59 +0400

    Changed to bitwise

--- bit_manipulation/1-print_binary.c
+++ bit_manipulation/1-print_binary.c
@@ -1,17 +1,6 @@
 #include "main.h"
 
 /**
- *
- */
-void print_num(int n)
-{
-	if (n == 0)
-		return;
-	print_num(n / 2);
-	printf("%d", n % 2);
-}
-
-/**
  * print_binary - Prints the binary representation of a number.
  * @n: Number that should be converted
  *
@@ -19,8 +8,25 @@ void print_num(int n)
  */
 void print_binary(unsigned long int n)
 {
-	if (n == 0)
-		printf("0");
-	else
-		print_num(n);
+	if (!n)
+	{
+		putchar(48);
+	}
+	print_bin(n);
+}
+
+/**
+ * print_num - Helper function.
+ * @n: Number that should be converted.
+ *
+ * Return: void.
+ */
+void print_num(unsigned long int n)
+{
+	if (!n)
+	{
+		return;
+	}
+	print_num(n >> 1);
+	putchar((n & 1) + 48);
 }
