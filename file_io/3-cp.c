24ef404ed86657a9b8a87f0e519f277aa0455448
samiribrh <samiribrahimov2277@gmail.com>
2024-04-07 19:22:03 +0400

    Betty style

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -11,16 +11,16 @@
  *	99 if can not be written to new file,
  *	100 if file descriptor can not be closed.
  */
- int main(int argc, char **argv)
- {
-	int red, wed, rfile, wfile, cl;
+int main(int argc, char **argv)
+{
+int red, wed, rfile, wfile, cl;
 	char *buffer;
 
 	if (argc != 3)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 	buffer = malloc(sizeof(char) * 1024);
 	if (buffer == NULL)
-		dprintf(STDERR_FILENO,"Error: Can't write to %s\n", argv[2]), exit(99);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 	rfile = open(argv[1], O_RDONLY);
 	red = read(rfile, buffer, 1024);
 	wfile = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
@@ -43,4 +43,4 @@
 	if (cl < 0)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", wfile), exit(100);
 	return (0);
- }
+}
