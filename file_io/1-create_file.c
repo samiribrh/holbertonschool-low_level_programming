c3b7312da5e363d2321402300b7de4cc01c73956
samiribrh <samiribrahimov2277@gmail.com>
2024-04-07 18:30:53 +0400

    Initial create_file function

---
+++ file_io/1-create_file.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * create_file - Creates a file.
+ * @filename: Name of the file
+ * @text_content: String to write to the file
+ *
+ * Return: 1,
+ *	-1 if fails.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, wed, len = 0;
+
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 600);
+	if (fd < 0)
+		return (-1);
+
+	if (text_content)
+		while (*(text_content + len))
+			len++;
+	else
+	{
+		close(fd);
+		return (-1);
+	}
+
+	wed = write(fd, text_content, len);
+	if (w < 0)
+		return (-1);
+	close(fd);
+	return (1);
+}
