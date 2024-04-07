814bcbb3e03bf07b97d76df525ab144c0222c9e0
samiribrh <samiribrahimov2277@gmail.com>
2024-04-07 18:06:45 +0400

    first commit

---
+++ file_io/README.md
@@ -0,0 +1,3 @@
+**Overview:**
+
+This directory consists of files that contains scripts written in C and includes file input, output.
---
+++ file_io/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int _putchar(char c);
+
+#endif
