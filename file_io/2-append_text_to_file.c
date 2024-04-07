dde61bfa9bbaac306320896e4da86e82568e7c1f
samiribrh <samiribrahimov2277@gmail.com>
2024-04-07 18:41:12 +0400

    Betty style

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -18,7 +18,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd < 0)
 		return (-1);
-	
+
 	if (text_content)
 	{
 		while (*(text_content + len))
